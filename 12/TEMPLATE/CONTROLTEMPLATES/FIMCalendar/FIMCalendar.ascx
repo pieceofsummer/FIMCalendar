bed050b867242065f8a85288eaa24cd86a163183
Alex Skalozub <pieceofsummer@gmail.com>
2013-03-11 02:53:53 +0400

    initial commit

---
+++ 12/TEMPLATE/CONTROLTEMPLATES/FIMCalendar/FIMCalendar.ascx
@@ -0,0 +1,2 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" Inherits="FIMCalendar.FIMCalendar, FIMCalendar, Version=1.0.0.0, Culture=neutral, PublicKeyToken=2738d3aeadf272f3"
+%>
---
+++ 12/TEMPLATE/CONTROLTEMPLATES/FIMCalendar/FIMCalendar.ascx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Microsoft.SharePoint;
+
+namespace FIMCalendar
+{
+    public partial class FIMCalendar : System.Web.UI.UserControl
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SPSecurity.RunWithElevatedPrivileges(delegate() {
+                using (SPSite site = new SPSite(SPContext.Current.Site.Url)) {
+                    using (SPWeb web = site.OpenWeb()) {
+
+                        string layoutRoot = "/_layouts/FIMCalendar";
+
+                        string[] cssFiles = new string[] {
+                            layoutRoot + "/CSS/jquery-ui.css"
+                        };
+
+                        string[] jsFiles = new string[] {
+                            layoutRoot + "/JS/jquery.js",
+                            layoutRoot + "/JS/jquery-ui.js",
+                            layoutRoot + "/JS/jquery-ui-i18n.js",
+                            layoutRoot + "/JS/fim-calendar.js"
+                        };
+
+
+                        for (int i = 0; i < cssFiles.Length; i++)
+                        {
+                            LiteralControl cssControl = new LiteralControl();
+                            cssControl.ID = "fimCalendarCSS" + i;
+                            cssControl.Text = string.Format("<link href=\"{0}\" rel=\"stylesheet\" type=\"text/css\" />", cssFiles[i]);
+
+                            if (FindControl(cssControl.ID) == null)
+                                Controls.Add(cssControl);
+                        }
+
+                        for (int i = 0; i < jsFiles.Length; i++)
+                        {
+                            if (!Page.ClientScript.IsClientScriptIncludeRegistered("fimCalendarJS" + i))
+                                Page.ClientScript.RegisterClientScriptInclude("fimCalendarJS" + i, jsFiles[i]);
+                        }
+                    }
+                }
+            });
+        }
+
+    }
+}
